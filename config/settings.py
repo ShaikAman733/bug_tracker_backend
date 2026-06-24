28ffe751d3cf69ca49803d4bdb3c9a8905a53dfe
Shaik Aman <shaikaman2411@gmai.com>
2026-06-24 17:54:51 +0530

    Fix Railway hosts

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [
+    ".up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -131,8 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.up.railway.app",
+]
